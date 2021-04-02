57c162051d99e264234cf0e7001343bd398df7a6
tr0b <trobin3000600@gmail.com>
2021-04-02 16:37:06 -0600

    Seventh Lab - Will add express JSON parser to Project Changes: - Added Express JSON parser, in order to successfully parse request body   json content - Added ClientsRoute to index js, in order to successfully consume all   Clients REST API endpoints

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-require('./database') // Database Connection
+app.use(express.json());
+
+require('./database'); // Database Connection
+const ClientsRoute = require('./routes/client.route')
+
+app.use('/', ClientsRoute)
 
 app.get('/', (req, res) => {
   res.send('Seventh Lab, Web Design II (Bus Services)')
 });
-
-
 
 app.listen(port, () => {
   console.log(`Listening on port ~> ${port}`)

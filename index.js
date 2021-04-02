87c73dc6aec39deaadcfb41e7c6bb7f18f3b547f
tr0b <trobin3000600@gmail.com>
2021-04-02 15:33:50 -0600

    Seventh Lab - Will add db connection import to index, port const Changes: - ADded db connection import to index file - Added port const to index file

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express();
+const port = 3000;
+
+require('./database') // Database Connection
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Seventh Lab, Web Design II (Bus Services)')
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ~> ${port}`)
 });
 

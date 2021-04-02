3bb6500061c87b29c6296ab7fe27c664d972495d
tr0b <trobin3000600@gmail.com>
2021-04-02 16:33:37 -0600

    Seventh Lab - Will fix clients routes Changes: - Removed wiki route comment - Fixed Client POST route not sending error message properly, as json - Fixed Client GET routes performing a circular reference due to   previous implementation - Fixed Client DELETE and PUT routes performing a circular reference due   to previous, buggy implementation

--- routes/client.route.js
+++ routes/client.route.js
@@ -1,5 +1,3 @@
-// wiki.js - Wiki route module.
-
 const express = require('express');
 
 const router = express.Router();
@@ -8,33 +6,39 @@ const router = express.Router();
 const Client = require('../models/client.js');
 
 // [POST] New Client
-router.post('/clients', (req, res) => {
+router.post('/clients', async (req, res) => {
   const client = new Client(req.body);
+  console.log(client);
   client.save().then(() => res.status(201).send())
-    .catch((e) => res.status(500).send(e.message));
+    .catch((e) => res.status(500).send({ err: e.message }));
 });
 
 // [GET] All Clients
-router.get('/clients', (req, res) => {
-  res.status(200).send(Client.find());
+router.get('/clients', async (req, res) => {
+  const clients = await Client.find();
+  res.status(200).json(clients);
 });
 
 // [GET] Client by _id
-router.get('/clients/:id', (req, res) => {
-  res.status(200).send(Client.find(req.params.id));
+router.get('/clients/:id', async (req, res) => {
+  const client = await Client.find({ _id: req.params.id })
+    .then(() => res.status(200).send(client))
+    .catch((e) => res.status(404).send({ err: e.message }));
 });
 
 // [DELETE] Client
 router.delete('/clients/:id', async (req, res) => {
   const { id } = req.params;
-  await Client.deleteOne({ _id: id }).then(() => res.send())
+  await Client.deleteOne({ _id: id })
+    .then(() => res.send())
     .catch((e) => res.status(404).send(e.message));
 });
 
 // [PUT] Client
 router.put('/clients/:id', async (req, res) => {
   const { id } = req.params;
-  await Client.updateOne({ _id: id }, req.body).then(() => res.send())
+  await Client.updateOne({ _id: id }, req.body)
+    .then(() => res.send())
     .catch((e) => res.status(404).send(e.message));
 });
 

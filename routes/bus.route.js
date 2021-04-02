db244c6d0fb8e3efbd6b7c9f65c60d67392993ce
tr0b <trobin3000600@gmail.com>
2021-04-02 17:26:09 -0600

    Seventh Lab - Will add Client populate, max clients restriction Changes Include: - Added max clients restriction in UPDATE (PATCH) operation - Added Client populate in bus routes GET operations, in order to show   all passengers information

--- routes/bus.route.js
+++ routes/bus.route.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 // Models
-const Bus = require('../models/bus.js');
+const Bus = require('../models/bus');
+const Client = require('../models/client')
 
 // [POST] New Bus
 router.post('/buses', async (req, res) => {
@@ -15,13 +16,13 @@ router.post('/buses', async (req, res) => {
 
 // [GET] All Buses
 router.get('/buses', async (req, res) => {
-  const buses = await Bus.find();
+  const buses = await Bus.find().populate('clients');
   res.status(200).json(buses);
 });
 
 // [GET] Bus by _id
 router.get('/buses/:id', async (req, res) => {
-  const bus = await Bus.find({ _id: req.params.id })
+  const bus = await Bus.find({ _id: req.params.id }).populate('clients')
     .then(() => res.status(200).send(bus))
     .catch((e) => res.status(404).send({ err: e.message }));
 });
@@ -34,10 +35,13 @@ router.delete('/buses/:id', async (req, res) => {
     .catch((e) => res.status(404).send(e.message));
 });
 
-// [PUT] Bus
-router.put('/buses/:id', async (req, res) => {
+// [PATCH] Bus
+router.patch('/buses/:id', async (req, res) => {
   const { id } = req.params;
-  await Bus.updateOne({ _id: id }, req.body)
+  if (req.body.clients.length > 20) {
+    return res.status(500).send({ err: 'A maximum of 20 clients are allowed per bus' });
+  }
+  return Bus.updateOne({ _id: id }, req.body)
     .then(() => res.send())
     .catch((e) => res.status(404).send(e.message));
 });
